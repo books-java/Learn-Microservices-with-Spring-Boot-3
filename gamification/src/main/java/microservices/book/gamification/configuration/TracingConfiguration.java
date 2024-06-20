8ca6e401558f1dffd5148441898598cdecc9e1c3
galkzaz <gamal.m.alkzaz@gmail.com>
2024-06-20 21:31:52 +0300

    chapter08 Distributed tracing

---
+++ gamification/src/main/java/microservices/book/gamification/configuration/TracingConfiguration.java
@@ -0,0 +1,16 @@
+package microservices.book.gamification.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+
+@Configuration
+public class TracingConfiguration {
+    // To have the @Observed support we need to register this aspect
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
+    }
+}
